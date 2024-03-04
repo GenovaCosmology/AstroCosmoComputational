41e3536616a91488bf7dcf19da8cc6705f2fad82
Massimiliano Chella <massimilianochella@MacBook-Pro-M.local>
2024-03-04 11:08:14 +0100

    integrale.py

---
+++ Students/MASSI/pyACC/Calculus/integrate.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+def integrale_trapezio(funzione, min, max, dx):
+    x=np.arange(min, max, dx)
+    fx=[]
+    tot=0
+    for i in x:
+        tot=tot+funzione(i)+funzione(i+dx)
+    return dx*tot/2
+
+
+print (integrale_trapezio(math.sin, 0, math.pi, 0.01))
+
+
+
