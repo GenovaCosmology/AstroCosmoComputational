61a75fc43e30cbfac96d077dfbfae41ffa6f9bd7
EdoardoMartinelli <edomarty2001@gmail.com>
2024-03-07 17:52:47 +0100

    cosmology modulus, exercise3 with test of comoving distances with different models

---
+++ Students/Martinelli_Edoardo/pyACC/Calculus/integrate.py
@@ -0,0 +1,32 @@
+import scipy
+import numpy as np
+import scipy.integrate as integrate
+from datetime import datetime
+from typing import Any
+import time
+
+def integrate_f(func,x_in=0,x_fin=0): # need to add ,deltax=0 if we want to use the trapezoid method
+    print("---")  
+    print("--> ",datetime.fromtimestamp(time.time()))
+    print("---")
+    
+    '''
+    This function integrates a function or a sample:
+
+    func: function or sample to integrate
+    x_in: initial point of the integration
+    x_fin: final point of the integration
+    
+    '''
+    bool =True
+    bool =callable(func)
+    if bool == True:
+        print("I'm integrating over a function, I used the quad method from scipy.")
+        return integrate.quad(func, x_in, x_fin)
+    else:
+        print("I'm integrating over a sample, I used the trapezoid method from scipy.")
+        x=np.linspace(x_in,x_fin,func.size)
+        integral= np.sum( ((func[1:]+func[0:-1])/2) * (x[1:]-x[0:-1]) )
+        return integral 
+        #return integrate.trapezoid(func,dx=(1/len(func)))
+        #with this method doesn't work the test, I don't know why. 
---
+++ Students/Martinelli_Edoardo/pyACC/cosmology/distances.py
@@ -0,0 +1,232 @@
+import numpy as np
+from ..Calculus import integrate_f
+c=299792 #km/s
+
+# in hubble we have all the information of the model
+
+def hubble(z, wde=-1, H0=67.0,omegaM=0.32,omegaLam=0.68,omegaGamma=0,omegaNu=0):
+    '''
+    hubble in units of km/s/Mpc
+    
+    WARNING: A non-zero value of curvature is given by the sum of the density parameters different from 1.
+
+    Parameters
+    ----------
+    z : float or array
+        Redshift
+    wde : float (optional->default: LCDM)
+        Dark energy zeldovich parameter
+    H0 : float (optional->default: LCDM)
+        Hubble constant
+    omegaM : float (optional->default: LCDM)
+        Matter and DM density
+    omegaLam : float (optional->default: LCDM)
+        Dark energy density
+    omegaGamma : float (optional->default: LCDM)
+        Radiation density
+    omegaNu : float (optional->default: LCDM)
+        Neutrino density
+    ----------
+    return : float or array
+        Hubble parameter
+    '''
+    omegaK=1-(omegaLam+omegaM+omegaGamma+omegaNu)
+    return np.sqrt(H0*H0*(omegaM*(1+z)**3+(omegaGamma+omegaNu)*(1+z)**4+omegaK*(1+z)**2+omegaLam*(1+z)**(3*(1+wde))))
+
+
+                          
+def comoving_distance(z, wde=-1, H0=67.0,omegaM=0.32, omegaLam=0.68,omegaGamma=0,omegaNu=0):
+    '''
+    comoving distance in Mpc
+
+    WARNING: This function uses the function hubble above,
+    please import it either from this module!
+    
+    Parameters
+    ----------
+    z : float or array
+        Redshift
+    wde : float (optional->default: LCDM)
+        Dark energy zeldovich parameter
+    H0 : float (optional->default: LCDM)
+        Hubble constant
+    omegaM : float (optional->default: LCDM)
+        Matter and DM density
+    omegaLam : float (optional->default: LCDM)
+        Dark energy density
+    omegaGamma : float (optional->default: LCDM)
+        Radiation density
+    omegaNu : float (optional->default: LCDM)
+        Neutrino density
+    ----------
+    return : float or array
+        Comoving distance
+    '''
+    integrand = lambda x: c/hubble(x, wde, H0, omegaM, omegaLam, omegaGamma, omegaNu)
+    if type(z) == float or type(z) == int:
+        return integrate_f(integrand, 0, z)[0]
+    else:
+        chiZ=np.zeros(z.size)
+        for z_i,Z in enumerate(z):
+            chiZ[z_i]=integrate_f(integrand, 0, Z)[0]
+        return chiZ
+
+
+def comoving_distance_transverse(z, wde=-1, H0=67.0,omegaM=0.32, omegaLam=0.68,omegaGamma=0,omegaNu=0):
+    '''
+    comoving distance transverse in Mpc
+
+    WARNING: This function uses the function comoving distance above,
+    please import it either from this module!
+
+    Parameters
+    ----------
+    z : float or array
+        Redshift
+    wde : float (optional->default: LCDM)
+        Dark energy zeldovich parameter
+    H0 : float (optional->default: LCDM)
+        Hubble constant
+    omegaM : float (optional->default: LCDM)
+        Matter and DM density
+    omegaLam : float (optional->default: LCDM)
+        Dark energy density
+    omegaGamma : float (optional->default: LCDM)
+        Radiation density
+    omegaNu : float (optional->default: LCDM)
+        Neutrino density
+    ----------
+    return : float or array
+        Comoving distance transverse
+    '''
+    omegaK=1-(omegaLam+omegaM+omegaGamma+omegaNu)
+    dh=c/H0
+    dc=comoving_distance(z, wde, H0, omegaM, omegaLam, omegaGamma, omegaNu)
+    if omegaK>0:
+        return (dh/np.sqrt(omegaK))*np.sinh((np.sqrt(omegaK)/dh)*dc)
+    if omegaK==0:
+        return dc
+    if omegaK<0:
+        return (dh/np.sqrt(-omegaK))*np.sin((np.sqrt(-omegaK)/dh)*dc)
+
+
+def proper_distance(z, wde=-1, H0=67.0,omegaM=0.32, omegaLam=0.68,omegaGamma=0,omegaNu=0):
+    '''
+    proper distance in Mpc
+
+    WARNING: This function uses the function comoving distance above,
+    please import it either from this module!
+    
+    Parameters
+    ----------
+    z : float or array
+        Redshift
+    wde : float (optional->default: LCDM)
+        Dark energy zeldovich parameter
+    H0 : float (optional->default: LCDM)
+        Hubble constant
+    omegaM : float (optional->default: LCDM)
+        Matter and DM density
+    omegaLam : float (optional->default: LCDM)
+        Dark energy density
+    omegaGamma : float (optional->default: LCDM)
+        Radiation density
+    omegaNu : float (optional->default: LCDM)
+        Neutrino density
+    ----------
+    return : float or array
+        Proper distance
+    '''
+    return comoving_distance(z, wde, H0,omegaM, omegaLam,omegaGamma,omegaNu)*(1+z)**-1
+
+
+def angular_diameter_distance(z, wde=-1, H0=67.0,omegaM=0.32, omegaLam=0.68,omegaGamma=0,omegaNu=0):
+    '''
+    angular diameter distance in Mpc
+
+    WARNING: This function uses the function hubble and comoving distance above,
+    please import them either from this module!
+    
+    Parameters
+    ----------
+    z : float or array
+        Redshift
+    wde : float (optional->default: LCDM)
+        Dark energy zeldovich parameter
+    H0 : float (optional->default: LCDM)
+        Hubble constant
+    omegaM : float (optional->default: LCDM)
+        Matter and DM density
+    omegaLam : float (optional->default: LCDM)
+        Dark energy density
+    omegaGamma : float (optional->default: LCDM)
+        Radiation density
+    omegaNu : float (optional->default: LCDM)
+        Neutrino density
+    ----------
+    return : float or array
+        Angular diameter distance
+    '''
+    return comoving_distance_transverse(z, wde, H0,omegaM, omegaLam,omegaGamma,omegaNu)*(1+z)**-1
+
+
+
+def luminosity_distance(z, wde=-1, H0=67.0,omegaM=0.32, omegaLam=0.68,omegaGamma=0,omegaNu=0):
+    '''
+    luminosity distance in Mpc
+
+    WARNING: This function uses the function hubble and comoving distance above,
+    please import them either from this module!
+    
+    Parameters
+    ----------
+    z : float or array
+        Redshift
+    wde : float (optional->default: LCDM)
+        Dark energy zeldovich parameter
+    H0 : float (optional->default: LCDM)
+        Hubble constant
+    omegaM : float (optional->default: LCDM)
+        Matter and DM density
+    omegaLam : float (optional->default: LCDM)
+        Dark energy density
+    omegaGamma : float (optional->default: LCDM)
+        Radiation density
+    omegaNu : float (optional->default: LCDM)
+        Neutrino density
+    ----------
+    return : float or array
+        luminosity distance
+    '''
+    return comoving_distance_transverse(z, wde, H0,omegaM, omegaLam,omegaGamma,omegaNu)*(1+z)
+
+def distance_modulus(z, wde=-1, H0=67.0,omegaM=0.32, omegaLam=0.68,omegaGamma=0,omegaNu=0):
+    '''
+    distance modulus
+
+    WARNING: This function uses the function luminosity distance above,
+    please import it either from this module!
+    
+    Parameters
+    ----------
+    z : float or array
+        Redshift
+    wde : float (optional->default: LCDM)
+        Dark energy zeldovich parameter
+    H0 : float (optional->default: LCDM)
+        Hubble constant
+    omegaM : float (optional->default: LCDM)
+        Matter and DM density
+    omegaLam : float (optional->default: LCDM)
+        Dark energy density
+    omegaGamma : float (optional->default: LCDM)
+        Radiation density
+    omegaNu : float (optional->default: LCDM)
+        Neutrino density
+    ----------
+    return : float or array
+        distance modulus
+    '''
+    return 5*np.log10(luminosity_distance(z, wde, H0,omegaM, omegaLam,omegaGamma,omegaNu))-5
+    
+    
