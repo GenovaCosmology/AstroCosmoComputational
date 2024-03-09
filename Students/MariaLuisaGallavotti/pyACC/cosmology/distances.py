7c7a1e210b41f18824bb0a07705ea5b018fdebd7
Maria Luisa Gallavotti <marigalla@MacbookriaLuisa.fritz.box>
2024-03-09 17:25:22 +0100

    integrate

--- Students/MariaLuisaGallavotti/pyACC/cosmology/distances.py
+++ Students/MariaLuisaGallavotti/pyACC/cosmology/distances.py
@@ -1,11 +1,11 @@
-from ..Calculus import integral
+from ..calculus import integral
 
-def integrand(z,H,OmegaM,H0):
+def integrand(z,H,OmegaM,OmegaR,w,H0):
     c=3*10**5
-    return c/H(z,OmegaM,H0)
+    return c/H(z,OmegaM,OmegaR,w,H0)
 
-def Dc(H,z,OmegaM,H0):
-    return integral(lambda t : integrand(t,H,OmegaM,H0),0,z,1000)
+def Dc(H,z,OmegaM,OmegaR,w,H0):
+    return integral(lambda t : integrand(t,H,OmegaM,OmegaR,w,H0),0,z,0.000001)
 
-#integral= lambda z : 1/H(z,OmegaM,H0)
+#integral= lambda z : 1/H(z,OmegaM,OmegaR,w,H0)
 #tutto quello che c'è dopo lambda è variabile, mentre gli altri parametri sono fissi
