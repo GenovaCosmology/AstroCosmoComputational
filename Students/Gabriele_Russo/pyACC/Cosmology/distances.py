362b103927d23e724c80899a1723847f14094415
Gabriele_Russo <gabryru01@gmail.com>
2024-03-13 09:37:58 +0100

    Saving files before merging with main

--- Students/Gabriele_Russo/pyACC/Cosmology/distances.py
+++ Students/Gabriele_Russo/pyACC/Cosmology/distances.py
@@ -11,5 +11,5 @@ def Integrand_comoving_distance(z, H, Omega_mat, H_0):
 #WARNING: trapezoid needs a function as argument, you can't just put c/H since it's flot/function; 
 #         this is why it has been necessary to define Integrand_comoving_distance
 def comoving_distance(z, H, Omega_mat, H_0):
-        return trapezoid(lambda z : Integrand_comoving_distance(z, H, Omega_mat, H_0), 0, z, 0.001) #with lambda z : function, we are reducing the function only with z as variable
+        return trapezoid(lambda z : Integrand_comoving_distance(z, H, Omega_mat, H_0), 0, z, 0.0001) #with lambda z : function, we are reducing the function only with z as variable
 
