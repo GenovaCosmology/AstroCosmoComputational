d2ba8a22e04ab75b87a00b4c2f329726267fbb66
guida_cla <guida_cla14@gmail.com>
2024-03-18 13:49:17 +0100

    class lesson4

--- Students/ClaudioGuida/pyACC/calculus/Function.py
+++ Students/ClaudioGuida/pyACC/calculus/Function.py
@@ -1,19 +1,31 @@
 #I wanna to create a class that takes a function as an argument and returns the value of the function in a point or in an array
 #and compute the integral of the function in a given interval and the derivative of the function in a point or a grid
+#try to extend it to mulidimensional functions
 
-from .integrate import *
+from ..calculus import integrate
 from scipy.misc import derivative
+from scipy.integrate import nquad
 
 class MyFunc:
     def __init__(self, func):  # Constructor method
         self.func = func  # Instance attribute
 
-    def value(self, x):
-        return self.func(x)
+    def value(self, *args):
+        return self.func(*args)
 
-    def integral(self, a, b):
-        return integrate.adaptive_quadrature(self.func, a, b)
+# it compute the integral only on the first variable
+#I want to compute the integral on the second variable, 
+    def integral(self, bounds, args = None):
+        return nquad(self.func, bounds, args=args)
+    
+    def derivative(self, point, h=1e-5, n_der=1, args = None):
+        return derivative(self.func, point, dx=h, n=n_der, args=args)
 
-    def derivative(self, x, h=1e-5, n_der=1):
-        return derivative(self.func, x, dx=h, n=n_der)
 
+#I wanna to implement a method that compute partial derivative of the function
+    def partial_derivative(self, point, var_index, h=1e-5, n_der=1, args=None):
+        def partial_func(args=args):
+            new_args = list(args)
+            new_args[var_index] += h
+            return (self.func(new_args) - self.func(args)) / h
+        return derivative(partial_func, point, dx=h, n=n_der, args=point)
