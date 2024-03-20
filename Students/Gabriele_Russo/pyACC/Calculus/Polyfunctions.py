4177f667c06cd1ab074ee3bb4a314c4cec4dbe00
Gabriele_Russo <gabryru01@gmail.com>
2024-03-20 08:40:30 +0100

    Routine saving

--- Students/Gabriele_Russo/pyACC/Calculus/Polyfunctions.py
+++ Students/Gabriele_Russo/pyACC/Calculus/Polyfunctions.py
@@ -1,22 +1,29 @@
 import sys
 sys.path.append("../")
 from Calculus import trapezoid
+from Calculus import simpsons_rule
 
 class Polyfunctions:
-
-    # Inizialization method (constructure)
+    ##************************************##
+    ##Inizialization method (constructure)
+    ##************************************##
     def __init__(self, function):
         self.function = function #self is telling the class to instantiate an object wich is the function I pass as parameter
-
-    #Methods of the class
+    
+    ##********************##
+    ##Methods of the class##
+    ##********************##
         
     #Evaluating function in one point
     def evaluate(self, argument):
         return self.function(argument)
     
     #Integrating function 
-    def integrate_function(self, x_min, x_max, delta_x):
+    def integrate_function_trapezoid(self, x_min, x_max, delta_x):
         return trapezoid(self.function, x_min, x_max, delta_x)
+    
+    def integrate_function_simpson_rule(self, x_min, x_max, n): #n is the number of intervalls
+        return simpsons_rule(self.function, x_min, x_max, n)
     
     #Derive function at first order
 
@@ -30,6 +37,12 @@ class Polyfunctions:
     
     #Derive function at second order
 
+    #Central derivative method
+    def central_difference_2_derivative(self, x, h):
+        return (self.function(x + h) - 2 * self.function(x) + self.function(x - h)) / (h**2)
+
+
+
 ##TEST
 def prova(x):
     return x*x
@@ -37,13 +50,17 @@ def prova(x):
 fun = Polyfunctions(prova)
 
 result  = fun.evaluate(5)
-result2 = fun.integrate_function(0,1,0.1) 
+result2 = fun.integrate_function_trapezoid(0,1,0.1) 
+result5 = fun.integrate_function_simpson_rule(0,1,100)
 result3 = fun.forward_difference_derivative(1,0.1)
 result4 = fun.central_difference_derivative(1,0.1)
+result6 = fun.central_difference_2_derivative(1,0.1)
 print(result)
 print(result2)
+print(result5)
 print(result3)
 print(result4)
+print(result6)
 
 
         
--- Students/Gabriele_Russo/pyACC/Cosmology/distances.py
+++ Students/Gabriele_Russo/pyACC/Cosmology/distances.py
@@ -11,5 +11,5 @@ def Integrand_comoving_distance(z, H, Omega_mat, H_0):
 #WARNING: trapezoid/simpson_rules needs a function as argument, you can't just put c/H since it's flot/function; 
 #         this is why it has been necessary to define Integrand_comoving_distance
 def comoving_distance(z, H, Omega_mat, H_0):
-        return simpsons_rule(lambda z : Integrand_comoving_distance(z, H, Omega_mat, H_0), 0, z, 100000) #with lambda z : function, we are reducing the function only with z as variable
+        return simpsons_rule(lambda z : Integrand_comoving_distance(z, H, Omega_mat, H_0), 0, z, 10000) #with lambda z : function, we are reducing the function only with z as variable
 
