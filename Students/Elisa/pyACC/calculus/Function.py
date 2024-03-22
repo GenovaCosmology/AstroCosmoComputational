997759b5eb319ea2e058ea5e8ef61ee570a83d0f
Elisa <elisalentini@LAPTOP-FRKD3D26.localdomain>
2024-03-22 10:29:19 +0100

    exercises from 1 to 3

--- Students/Elisa/pyACC/calculus/Function.py
+++ Students/Elisa/pyACC/calculus/Function.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy
-import scipy.integrate as integrate
+from scipy.integrate import quad
 from sympy import *
 
 
@@ -11,7 +11,7 @@ class Function:
     """
 
 
-    def __init__(self,func,x0,x_min,x_max,deltax):
+    def __init__(self,func):
         
         """
         self makes the class see all the function defined in the class
@@ -20,18 +20,18 @@ class Function:
         self.func = func
         
         
-        def f(self,x0):
-            myfunc = lambda t : self.func(t)
-            return myfunc(x0)
+    def f(self,x0):
+        myfunc = lambda t : self.func(t)
+        return myfunc(x0)
 
 
-        def integrate(self,x_min,x_max):
-            myfunc = lambda t : self.func(t)
-            return integrate.quad(myfunc, x_min,x_max)
+    def integration(self,x_min,x_max):
+        myfunc = lambda t : self.func(t)
+        return quad(myfunc, x_min,x_max)[0]
 
 
-        def derivative(self,n):
-            # n = derivative's degree
-            myfunc = lambda t : self.func(t)
-            return diff(myfunc,t,n)
+    def derivation(self,n):
+        # n = derivative's degree
+        myfunc = lambda t : self.func(t)
+        return diff(myfunc,t,n)
         
