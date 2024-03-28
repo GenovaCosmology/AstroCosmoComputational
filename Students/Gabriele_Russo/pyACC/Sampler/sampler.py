b976f87d2a1f23e6fe703dcc82fac64ec1086056
Gabriele_Russo <gabryru01@gmail.com>
2024-03-28 10:15:41 +0100

    I have no idea of what I am doing in the exercise with PDFs and CDFs

---
+++ Students/Gabriele_Russo/pyACC/Sampler/sampler.py
@@ -0,0 +1,23 @@
+#**********************#
+#Proposal Distributions#
+#**********************#
+
+#Uniform distribution
+def uniform(x, min_x, max_x):
+    if x >= min_x and x <= max_x:
+        return 1 / (max_x - min_x) #to have normalization
+    else:
+        return 0
+    
+
+
+#*******************#
+#Sampling alghoritms#
+#*******************#
+    
+#Rejection Sampling
+def rejection_sampling():
+    return 
+
+
+ 
