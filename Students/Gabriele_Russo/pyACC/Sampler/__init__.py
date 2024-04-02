02d87c750db1bafdcd4cdebb47c74d17bfdcb74a
Gabriele_Russo <gabryru01@gmail.com>
2024-04-02 10:09:29 +0200

    MCMC mistake point 2b file sampler

--- Students/Gabriele_Russo/pyACC/Sampler/__init__.py
+++ Students/Gabriele_Russo/pyACC/Sampler/__init__.py
@@ -1,2 +1,3 @@
 from .sampler import rejection_sampling
 from .sampler import rejection_sampling_nd
+from .sampler import metropolis_hastings
--- Students/Gabriele_Russo/pyACC/Sampler/sampler.py
+++ Students/Gabriele_Russo/pyACC/Sampler/sampler.py
@@ -58,7 +58,7 @@ def rejection_sampling_nd(num_samples, n, ranges, pdf_func):
 
     Returns:
     - np.array(samples): array (sample points converted in an array)
-    - efficiency: int (how many points were generated in total)
+    - efficiency_conunter: int (how many points were generated in total)
     '''
     samples = []
     efficiency_counter = 0
@@ -73,4 +73,47 @@ def rejection_sampling_nd(num_samples, n, ranges, pdf_func):
         if np.random.uniform(0, 1) < pdf_func(*point):
             samples.append(point)
     return np.array(samples), efficiency_counter
- 
+
+# Metropolis-Hastings algorithm
+def metropolis_hastings(target_distribution, initial_state, num_samples, proposal_distribution, proposed_sd):
+    '''
+    This function wants:
+    - target_distribution: function (target pdf that we want to compute)
+    - initial_state: float (initial state of the Markov chain)  
+    - num_samples: int (number of samples to generate)
+    - proposal_distribution: function (proposal distribution function for generation of samples)
+    - proposed_sd: float (standard distribution for proposal pdf)
+
+    Returns:
+    - np.array(samples): array (sample points converted in an array)
+    - efficiency_conunter: int (how many points were generated in total)
+    '''
+    #Preparing a list for samples putting the initial state as first element
+    samples = [initial_state]
+    #Setting the current state to the initial state
+    current_state = initial_state
+
+    efficiency_counter = 0
+    
+    for _ in range(num_samples):
+        efficiency_counter += 1
+        # Step 2a: Generate a candidate x' using the proposal distribution
+        proposed_state = proposal_distribution(current_state, proposed_sd)
+        
+        # Step 2b: Calculate the acceptance ratio alpha as in the scketch of MCMC algorithm
+        numerator = target_distribution(proposed_state) * proposal_distribution(current_state, proposed_sd)
+        denominator = target_distribution(current_state) * proposal_distribution(proposed_state, proposed_sd)
+        alpha = min(1, numerator / denominator)
+        
+        # Step 2c: Generate a uniform random number u  between 0 and 1 
+        u = np.random.rand()
+        
+        # Step 2d: Accept or reject the candidate based on the acceptance ratio
+        if u <= alpha:
+            current_state = proposed_state
+        
+        samples.append(current_state)
+    
+    return np.array(samples), efficiency_counter
+
+        
