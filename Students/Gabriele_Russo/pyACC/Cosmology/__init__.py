0b7e0b17c88e616d8d1d4fac5c04227a23eb68e2
Gabriele_Russo <gabryru01@gmail.com>
2024-04-22 08:55:30 +0200

    Pre lesson

--- Students/Gabriele_Russo/pyACC/Cosmology/__init__.py
+++ Students/Gabriele_Russo/pyACC/Cosmology/__init__.py
@@ -1,3 +1,4 @@
 from .distances import Integrand_comoving_distance
 from .distances import comoving_distance
 from .distances import CosmologicalDistances
+from .power_spectrum_estimator import power_spectrum_estimator
---
+++ Students/Gabriele_Russo/pyACC/Cosmology/power_spectrum_estimator.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def power_spectrum_estimator(data_array):
+    '''
+    Implementing Power Spectrum Estimator starting from 
+    2 Point Correletion Function Estimator designed by Landy and Szalay in 1993
+
+    Input:
+    - data_array: np array
+    '''
+
+    #2PCFE
+
+    for i in range(0, ):
