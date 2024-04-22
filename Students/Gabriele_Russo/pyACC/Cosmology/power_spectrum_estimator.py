0a8af5411132857049da1daaf1bb6a1542e7bab4
Gabriele_Russo <gabryru01@gmail.com>
2024-04-22 09:04:05 +0200

    Last commit before lesson

--- Students/Gabriele_Russo/pyACC/Cosmology/power_spectrum_estimator.py
+++ Students/Gabriele_Russo/pyACC/Cosmology/power_spectrum_estimator.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def power_spectrum_estimator(data_array):
+def power_spectrum_estimator(data_array, min_corr, max_corr):
     '''
     Implementing Power Spectrum Estimator starting from 
     2 Point Correletion Function Estimator designed by Landy and Szalay in 1993
@@ -10,5 +10,11 @@ def power_spectrum_estimator(data_array):
     '''
 
     #2PCFE
+    DD = 0
+    for i in range(0, len(data_array)):
+        for j in range(0, len(data_array)):
+            r = abs(data_array[i]-data_array[j])
+        
+        if r>=min_corr and r<=max_corr:
+            DD += 1
 
-    for i in range(0, ):
