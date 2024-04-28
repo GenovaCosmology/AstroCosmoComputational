8538a038946002fd768dd39d6b9a2506ea93dc59
guida_cla <guida_cla14@gmail.com>
2024-04-28 12:36:56 +0200

    function class

--- Students/ClaudioGuida/pyACC/calculus/Function.py
+++ Students/ClaudioGuida/pyACC/calculus/Function.py
@@ -13,19 +13,50 @@ class MyFunc:
     def value(self, *args):
         return self.func(*args)
 
-# it compute the integral only on the first variable
-#I want to compute the integral on the second variable, 
-    def integral(self, bounds, args = None):
-        return nquad(self.func, bounds, args=args)
-    
-    def derivative(self, point, h=1e-5, n_der=1, args = None):
+    # it compute the integral only on the first variable
+    # I want to compute the integral on the second variable
+    def integral(self, bounds, n=1, args=None):
+        if n != 1:
+            return nquad(self.func, bounds, args=args)[0]
+        return integrate.adaptive_quadrature(self.func, bounds[0][0], bounds[0][1])
+
+    def derivative(self, point, h=1e-5, n_der=1, *args):
         return derivative(self.func, point, dx=h, n=n_der, args=args)
 
 
 #I wanna to implement a method that compute partial derivative of the function
-    def partial_derivative(self, point, var_index, h=1e-5, n_der=1, args=None):
-        def partial_func(args=args):
+    def partial_derivative(self, point, var_index, h=1e-5, n_der=1):
+        def partial_func(x, *args):
             new_args = list(args)
-            new_args[var_index] += h
-            return (self.func(new_args) - self.func(args)) / h
-        return derivative(partial_func, point, dx=h, n=n_der, args=point)
+            new_args[var_index] = x
+            return self.func(*new_args)
+        #I want to evaluate the partial derivative of the function in a point, with respect to the variable var_index, i have to use value method
+        return derivative(partial_func, point[var_index], dx=h, n=n_der, args=point)
+    
+
+    def gradient(self, point, h=1e-5, n_der=1):
+        return [self.partial_derivative(point, i, h, n_der) for i in range(len(point))]
+    
+
+    def partial_derivative_at_point(self, variable, point, h=1e-6):
+        if not callable(self.func):
+            raise ValueError("The function is not callable.")
+        
+        if variable not in self.func.__code__.co_varnames:
+            raise ValueError(f"'{variable}' is not a variable in the function.")
+
+        index = self.func.__code__.co_varnames.index(variable)
+
+        def perturbed_point_plus(point):
+            perturbed_point = list(point)
+            perturbed_point[index] += h
+            return tuple(perturbed_point)
+
+        def perturbed_point_minus(point):
+            perturbed_point = list(point)
+            perturbed_point[index] -= h
+            return tuple(perturbed_point)
+
+        partial_derivative = (self.value(*perturbed_point_plus(point)) - self.value(*perturbed_point_minus(point))) / (2 * h)
+
+        return partial_derivative
