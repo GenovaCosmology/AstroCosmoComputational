983fffdf3ab091b81364d8f1906aa5f4b9002a40
Gabriele_Russo <gabryru01@gmail.com>
2024-05-02 09:30:49 +0200

    Problem with pyACC module

--- Students/Gabriele_Russo/pyACC/Cosmology/power_spectrum_estimator.py
+++ Students/Gabriele_Russo/pyACC/Cosmology/power_spectrum_estimator.py
@@ -1,3 +1,6 @@
+'''
+TEST PROGRAM for this function IMPLEMENTED in exercise1.ipynb Lesson 8
+'''
 import numpy as np
 import math as m
 import matplotlib as plt
@@ -13,7 +16,7 @@ def power_spectrum_estimator(Delta_X, Delta_Y, Delta_Z, delta, n):
     - n      : int, for rescaling binning pf K_modulus as multiples of the foundamental frequency
 
     Output:
-    -
+    - points_power_spectrum: numpy array containing the power spectrum
     '''
     #Computing step along axis
     delta_x = Delta_X[1] - Delta_X[0] 
@@ -36,6 +39,12 @@ def power_spectrum_estimator(Delta_X, Delta_Y, Delta_Z, delta, n):
     X_fourier, Y_fourier, Z_fourier = np.meshgrid(freq_x, freq_y, freq_z, indexing='ij')
     # Now X_fourier, Y_fourier, and Z_fourier contain the corresponding coordinates in Fourier space
     
+    # Compute the squared magnitude of the Fourier coefficients
+    power_spectrum = np.abs(data_transform)**2
+
+    #Normalize the power spectrum ensuring its values represent the power per unit area in the frequency domain
+    power_spectrum /= np.prod(delta.shape)
+
     #Modulus of distances in fourier space
     K_modulus = np.sqrt(X_fourier**2 + Y_fourier**2 + Z_fourier**2)
 
@@ -56,3 +65,21 @@ def power_spectrum_estimator(Delta_X, Delta_Y, Delta_Z, delta, n):
     plt.ylabel('Frequency')
     plt.title('Histogram of K Modulus with Fixed Size Bins')
     plt.show()
+
+    #Power Spectrum
+    points_power_spectrum = np.zeros(num_bins+1)
+
+    for  i in range(len(bin_indices.flatten())):
+        points_power_spectrum[bin_indices[i]-1] += power_spectrum.flatten()[i]
+        #Digitize starts from 1! You must subtract 1!
+
+    # Plot the points_power_spectrum
+    plt.plot(np.arange(0, K_modulus.max() + k_f, k_f), points_power_spectrum, marker='o', linestyle='-')
+    plt.xlabel('Bin Index in K')
+    plt.ylabel('Power Spectrum')
+    plt.title('Power Spectrum')
+    plt.grid(True)
+    plt.show()
+
+    return points_power_spectrum
+
