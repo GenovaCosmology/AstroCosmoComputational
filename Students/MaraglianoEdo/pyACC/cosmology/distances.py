44601db7517379395fb875d86a56df45a10072b7
edogenova <edoardo.maragliano@gmail.com>
2024-05-03 14:47:14 +0200

    updated pyACC.cosmology distances and distances_func

--- Students/MaraglianoEdo/pyACC/cosmology/distances.py
+++ Students/MaraglianoEdo/pyACC/cosmology/distances.py
@@ -4,6 +4,23 @@ from scipy.constants import c as speed_of_light
 
 speed_of_light = speed_of_light/1000 ### speed of light from m/s to km/s
 
+def LambdaCDMCosmology():
+    lambdaCDM_parameters = {
+    'H0': 67,            # Hubble constant (km/s/Mpc)
+    'Om_m': 0.319,      # Matter density parameter
+    'Om_lambda': 0.681, # Dark energy density parameter
+    'Om_rad': 0.0,
+    'Om_nu': 0.0,
+    'Om_k': 0.0,      
+    'w0': -1.0,            # Dark energy equation of state parameter (constant)
+    'wa': 0.0              # Dark energy equation of state parameter (time-dependent)
+    }
+    return lambdaCDM_parameters
+
+def hubble_function(z, H0=67, Om_m=0.319, Om_lambda=0.681, Om_rad=0, Om_nu=0, Om_k=0, w0=-1.0):
+    return H0*np.sqrt(Om_m*(1+z)**3+Om_rad*(1+z)**4+Om_k*(1+z)**2+Om_lambda*(1+z)**(3*(1+w0)))
+
+
 class CosmologicalDistances:
     """
     A class to compute cosmological distances using different distance measures.
@@ -101,8 +118,15 @@ class CosmologicalDistances:
         """
 
         # Calculate distance modulus
-        distance_modulus = 5.0 * (np.log10(luminosity_distance) - 1.0) 
+        distance_modulus = 5.0 * (np.log10(luminosity_distance)-1) 
 
         return distance_modulus
 
+    def hubble_distance(self, redshift):
+        return speed_of_light/self.hubble_function(redshift)
 
+    def transverse_comoving_distance(self, redshift):
+        return self.comoving_distance(redshift)
+    
+    def isotropic_volume_distance(self,redshift):
+        return np.power(redshift * self.transverse_comoving_distance(redshift)**2 * self.hubble_distance(redshift), 1./3)
--- Students/MaraglianoEdo/pyACC/cosmology/distances_func.py
+++ Students/MaraglianoEdo/pyACC/cosmology/distances_func.py
@@ -7,23 +7,23 @@ speed_of_light = speed_of_light/1000 ### speed of light from m/s to km/s
 def LambdaCDMCosmology():
     lambdaCDM_parameters = {
     'H0': 67,            # Hubble constant (km/s/Mpc)
-    'Omega_m': 0.319,      # Matter density parameter
-    'Omega_lambda': 0.681, # Dark energy density parameter
-    'Omega_b': 0.049,      # Baryonic matter density parameter
-    'sigma_8': 0.83,       # Root-mean-square amplitude of mass fluctuations in a sphere of radius 8 Mpc/h
-    'n_s': 0.965,          # Spectral index of the primordial power spectrum
+    'Om_m': 0.319,      # Matter density parameter
+    'Om_lambda': 0.681, # Dark energy density parameter
+    'Om_rad': 0.0,
+    'Om_nu': 0.0,
+    'Om_k': 0.0,      
     'w0': -1.0,            # Dark energy equation of state parameter (constant)
     'wa': 0.0              # Dark energy equation of state parameter (time-dependent)
     }
     return lambdaCDM_parameters
 
-def hubble_function(z, H0=67, Om_m=0.3, Om_lambda=0.7, Om_rad=0, Om_nu=0, Om_k=0, w0=-1.0):
+def hubble_function(z, H0=67, Om_m=0.319, Om_lambda=0.681, Om_rad=0, Om_nu=0, Om_k=0, w0=-1.0, wa=0.0):
     return H0*np.sqrt(Om_m*(1+z)**3+Om_rad*(1+z)**4+Om_k*(1+z)**2+Om_lambda*(1+z)**(3*(1+w0)))
 
 
-def comoving_distance(z, H0=67, Om_m=0.3, Om_lambda=0.7, Om_rad=0, Om_nu=0, Om_k=0):
+def comoving_distance(z, H0=67, Om_m=0.319, Om_lambda=0.681, Om_rad=0, Om_nu=0, Om_k=0, w0=-1.0, wa=0.0):
 
-    integrand = lambda z: speed_of_light/hubble_function(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k )
+    integrand = lambda z: speed_of_light/hubble_function(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k , w0, wa)
 
     if(isinstance(z, (int, float))):
         integral = quad(integrand, 0.0, z)[0]
@@ -34,18 +34,18 @@ def comoving_distance(z, H0=67, Om_m=0.3, Om_lambda=0.7, Om_rad=0, Om_nu=0, Om_k
     return integral
 
 
-def angular_diameter_distance(z, H0=67, Om_m=0.3, Om_lambda=0.7, Om_rad=0, Om_nu=0, Om_k=0):
-    return comoving_distance(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k)/(1+z)
+def angular_diameter_distance(z, H0=67, Om_m=0.319, Om_lambda=0.681, Om_rad=0, Om_nu=0, Om_k=0, w0=-1.0, wa=0.0):
+    return comoving_distance(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k, w0, wa)/(1+z)
 
     
-def luminosity_distance(z, H0=67, Om_m=0.3, Om_lambda=0.7, Om_rad=0, Om_nu=0, Om_k=0):
-    return comoving_distance(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k)*(1+z)
+def luminosity_distance(z, H0=67, Om_m=0.319, Om_lambda=0.681, Om_rad=0, Om_nu=0, Om_k=0, w0=-1.0, wa=0.0):
+    return comoving_distance(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k, w0, wa)*(1+z)
         
 
-def distance_modulus_from_redshift(z, H0=67, Om_m=0.3, Om_lambda=0.7, Om_rad=0, Om_nu=0, Om_k=0):
+def distance_modulus_from_redshift(z, H0=67, Om_m=0.319, Om_lambda=0.681, Om_rad=0, Om_nu=0, Om_k=0, w0=-1.0, wa=0.0):
 
     # Calculate luminosity distance (in Mpc)
-    dL = luminosity_distance(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k)
+    dL = luminosity_distance(z, H0, Om_m, Om_lambda, Om_rad, Om_nu, Om_k, w0, wa)
 
     # Calculate distance modulus
     distance_modulus = 5.0 * (np.log10(dL) - 1.0) 
@@ -59,5 +59,8 @@ def distance_modulus_from_luminosity_distance(luminosity_distance):
     distance_modulus = 5.0 * (np.log10(luminosity_distance) - 1.0) 
 
     return distance_modulus
+
+def hubble_distance(redshift):
+    return speed_of_light/hubble_function(redshift)
 
 
